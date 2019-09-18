285a98d8aceb0e4baf3aeb1fa71f5c01e45b2998
Thomas Briggs <thomas.briggs333@gmail.com>
2019-09-18 11:23:01 +0100

    validParentheses

---
+++ Code Wars/Valid Parentheses/validParentheses.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+ int main() {
+     char test[] = "hello";
+     for (int i = 0; i < 7; i++)
+     {
+         cout << test[i] << endl;
+     }
+     
+
+     return 0;
+ }
