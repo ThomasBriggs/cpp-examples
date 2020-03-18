1ef8f3b32e19fdc4b0f5ed5c5d993492632cb0a0
Thomas Briggs <thomas.briggs333@gmail.com>
2020-03-18 21:36:32 +0000

    QuickSortV2

--- QuickSort/src/QuickSortV2.cpp
+++ QuickSort/src/QuickSortV2.cpp
@@ -1,19 +1,64 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 void quickSort(int array[], int start, int end)
 {
-    int left = start;
-    int right = end;
-    int pivot = (start + end) / 2;
 
-    cout << pivot << endl;
+    if (start < end)
+    {
+
+        int left = start;
+        int right = end;
+        int length = start + end;
+        int pivot = length / 2;
+
+        while (left != right)
+        {
+
+            left = start;
+            right = end;
+
+            while (array[left] < array[pivot])
+            {
+                left++;
+            }
+
+            while (array[right] > array[pivot])
+            {
+                right--;
+            }
+
+            int temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+        }
+
+        quickSort(array, start, pivot - 1);
+        quickSort(array, pivot + 1, end);
+    }
+}
+
+void quickSort(int array[], int length)
+{
+    quickSort(array, 0, length - 1);
 }
 
 int main(int argc, char const *argv[])
 {
-    int testArray[] = {12, 29, 7, 30, 17};
-    cout << "Hello, World!" << endl;
+    int length = 50;
+    int testArray[length];
+    for (int i = 0; i < length; i++)
+    {
+        testArray[i] = rand() % 1000;
+    }
+
+    quickSort(testArray, length);
+    for (auto &&i : testArray)
+    {
+        cout << i << ", ";
+    }
+    cout << endl;
     return 0;
 }
