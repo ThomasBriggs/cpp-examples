6e4691050d10a2691d73efac7fabc3df482e50c5
Thomas Briggs <thomas.briggs333@gmail.com>
2020-03-18 17:32:23 +0000

    QuickSortVs

---
+++ QuickSort/src/QuickSortV2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void quickSort(int array[], int start, int end)
+{
+    int left = start;
+    int right = end;
+    int pivot = (start + end) / 2;
+
+    cout << pivot << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    int testArray[] = {12, 29, 7, 30, 17};
+    cout << "Hello, World!" << endl;
+    return 0;
+}
