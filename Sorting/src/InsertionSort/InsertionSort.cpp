ac219a734e612693b3cd24ff291f6caedcf83196
Thomas Briggs <thomas.briggs333@gmail.com>
2020-03-22 17:52:20 +0000

    Moved swap function location

--- Sorting/src/InsertionSort/InsertionSort.cpp
+++ Sorting/src/InsertionSort/InsertionSort.cpp
@@ -11,13 +11,6 @@ void fillRandom(int *arr, int amount)
     }
 }
 
-void swap(int *arr, int index1, int index2)
-{
-    int temp = arr[index1];
-    arr[index1] = arr[index2];
-    arr[index2] = temp;
-}
-
 void print(int *arr, int length)
 {
     for (size_t i = 0; i < length; i++)
@@ -25,6 +18,13 @@ void print(int *arr, int length)
         std::cout << arr[i] << ", ";
     }
     std::cout << std::endl;
+}
+
+void swap(int *arr, int index1, int index2)
+{
+    int temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
 }
 
 void InsertionSort(int *arr, int length)
