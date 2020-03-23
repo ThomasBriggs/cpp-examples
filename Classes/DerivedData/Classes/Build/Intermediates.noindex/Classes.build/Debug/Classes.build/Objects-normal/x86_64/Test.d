50540faeae4ee1492bd2ff5711ca6c231918f254
Thomas Briggs <thomas.briggs333@gmail.com>
2020-03-23 12:40:02 +0000

    git-update

--- Classes/DerivedData/Classes/Build/Intermediates.noindex/Classes.build/Debug/Classes.build/Objects-normal/x86_64/Test.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/thomasbriggs/Google\ Drive/Programming/C++/Classes/Classes/Test.cpp \
-  /Users/thomasbriggs/Google\ Drive/Programming/C++/Classes/Classes/Test.hpp
--- Classes/DerivedData/Classes/Build/Intermediates.noindex/Classes.build/Debug/Classes.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/thomasbriggs/Google\ Drive/Programming/C++/Classes/Classes/main.cpp
