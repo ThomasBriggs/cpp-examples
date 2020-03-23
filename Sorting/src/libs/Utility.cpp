853f00b3d71743cc77351a43928a31393f78cf32
Thomas Briggs <thomas.briggs333@gmail.com>
2020-03-23 02:53:35 +0000

    Utility Library

--- Sorting/src/libs/Utility.cpp
+++ Sorting/src/libs/Utility.cpp
@@ -1,9 +1,9 @@
-#include <Utility.h>
+#include "Utility.h"
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 
-void fillRandom(int *arr, int amount)
+void Utility::fillRandom(int *arr, int amount)
 {
     srand((unsigned)time(0));
     for (size_t i = 0; i < amount; i++)
@@ -12,14 +12,14 @@ void fillRandom(int *arr, int amount)
     }
 }
 
-void swap(int *arr, int index1, int index2)
+void Utility::swap(int *arr, int index1, int index2)
 {
     int temp = arr[index1];
     arr[index1] = arr[index2];
     arr[index2] = temp;
 }
 
-void print(int *arr, int length)
+void Utility::print(int *arr, int length)
 {
     for (size_t i = 0; i < length; i++)
     {
