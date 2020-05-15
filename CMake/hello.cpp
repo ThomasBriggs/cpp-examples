f23e5a3f396d05416aa92f0a15f16e84a2483ef0
Thomas Briggs <thomas.briggs333@gmail.com>
2020-05-15 17:32:18 +0100

    Gui and CMake

---
+++ CMake/hello.cpp
@@ -0,0 +1,8 @@
+#include "hello.hpp"
+
+#include <iostream>
+
+void hello::say_hello(){
+    // Say hello
+    std::cout << "Hello, World!" << std::endl;
+}
---
+++ CMake/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "hello.hpp"
+
+int main(int argc, char const *argv[])
+{
+    using namespace std;
+    hello::say_hello();
+    return 0;
+}
