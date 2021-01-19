8d11a6a87f9a4d245a788077020480694e2a991e
Thomas Briggs <thomas.briggs333@gmail.com>
2021-01-19 16:09:41 +0000

    Testing iterators

--- ExpressionParser/test/test.cpp
+++ ExpressionParser/test/test.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include "Expression.hpp"
+#include <vector>
+#include <cmath>
+#include <algorithm>
 
 int main(int argc, char const *argv[])
 {
-    Expression a = Expression::parse("2 + 5 * 10");
-    std::cout << a.eval() << '\n';
+    
+    using namespace std;
 
+    vector<int> a = {1, 2, 3, 4, 5, 6, 7, 8, 9}; 
+    auto test = min_element(a.begin(), a.end(),
+        [](int a, int b){
+            return a < b;
+        });
+
+    cout << *(a.end()-1) << '\n';
+    cout << *test << '\n';
+
+    int i = 10;
+    int* i_ptr = &i;
+    cout << *i_ptr << '\n';
     return 0;
 }
