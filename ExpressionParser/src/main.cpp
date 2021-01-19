4ef7faa95dd57c6ebefc32ed3ddcee2c5ec9efd8
Thomas Briggs <thomas.briggs333@gmail.com>
2021-01-19 23:07:11 +0000

    Removed main

--- ExpressionParser/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Expression.hpp"
-
-int main(int argc, char const *argv[])
-{
-    // Expression a = Expression::parse("1 + 2");
-    // std::cout << Expression::parse(argv[1]).eval() << std::endl;
-    return 0;
-}
