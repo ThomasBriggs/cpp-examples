97a7632754891d7798fa4079823c198f60af9c81
Thomas Briggs <thomas.briggs333@gmail.com>
2021-01-20 01:18:07 +0000

    Optimised parseRec

--- ExpressionParser/src/Expression.cpp
+++ ExpressionParser/src/Expression.cpp
@@ -31,8 +31,6 @@ Expression Expression::parse(std::string s)
     return r;
 }
 
-
-
 constexpr Expression::op_precedence Expression::precedence(char ch)
 {
     op_precedence val = op_precedence::NON_OP;
@@ -49,66 +47,48 @@ constexpr Expression::op_precedence Expression::precedence(char ch)
     case '^':
         val = op_precedence::HIGH;
         break;
-    case '(':
-    case ')':
-        val = op_precedence::BRACKET;
-        break;
     default:
         break;
     }
     return val;
 }
 
-// Expression Expression::parseRec(const std::string &s) {
-//     std::string::const_iterator lowest = std::min_element(s.begin(), s.end(), 
-//         [](const char &a, const char &b){
-//             return precedence(a) < precedence(b); 
-//         });
-//     if (precedence(*lowest) == op_precedence::NON_OP) {
-//         return Expression(s, nullptr, nullptr);
-//     } else {
-//         return Expression(std::string(1, *lowest),
-//                           std::make_unique<Expression>(parseRec(s.substr(0, lowest-s.begin()))),
-//                           std::make_unique<Expression>(parseRec(s.substr(lowest-s.begin() + 1)))
-//                           );
-//     }
-// };
+Expression Expression::parseRec(const std::string &s)
+{
 
-Expression Expression::parseRec(const std::string &s) {
-    
-    int indexLowestOp = -1;
-    bool inBracket = false;
+    std::string::const_iterator lowestOp = s.begin();
+    int bracketCount = 0;
     bool bracketFound = false;
 
-    for (size_t i = 0; i < s.length(); i++)
+    for (std::string::const_iterator i = s.begin(); i != s.end(); i++)
     {
-        auto currentPres = precedence(s[i]);
-        if (currentPres == op_precedence::BRACKET) {
-            inBracket = !inBracket;
+        switch (*i)
+        {
+        case '(':
+            bracketCount++;
             bracketFound = true;
+            break;
+        case ')':
+            bracketCount--;
+            break;
         }
-        if (!inBracket){
-            if (indexLowestOp == -1 && currentPres != op_precedence::NON_OP
-                && currentPres != op_precedence::BRACKET) {
-                indexLowestOp = i;
-            } else {
-                if (currentPres <= precedence(s[indexLowestOp]) 
-                    && precedence(s[indexLowestOp]) != op_precedence::NON_OP
-                    && currentPres != op_precedence::BRACKET)
-                    indexLowestOp = i;
-            }
-        }
+        if (bracketCount == 0)
+            if (precedence(*i) < precedence(*lowestOp))
+                lowestOp = i;
     }
-    
-    
-    if (bracketFound && indexLowestOp == -1) {
-        return parseRec(s.substr(1, s.length()-2));
-    } else if (indexLowestOp == -1) {
+
+    if (precedence(*lowestOp) == op_precedence::NON_OP && bracketFound)
+    {
+        return parseRec(s.substr(1, s.length() - 2));
+    }
+    else if (precedence(*lowestOp) == op_precedence::NON_OP)
+    {
         return Expression(s, nullptr, nullptr);
-    } else {
-        return Expression(std::string(1, s[indexLowestOp]),
-                          std::make_unique<Expression>(parseRec(s.substr(0, indexLowestOp))),
-                          std::make_unique<Expression>(parseRec(s.substr(indexLowestOp+1)))
-                          );
+    }
+    else
+    {
+        return Expression(std::string(1, *lowestOp),
+                          std::make_unique<Expression>(parseRec(s.substr(0, lowestOp - s.begin()))),
+                          std::make_unique<Expression>(parseRec(s.substr(lowestOp - s.begin() + 1))));
     }
 };
--- ExpressionParser/src/Expression.hpp
+++ ExpressionParser/src/Expression.hpp
@@ -8,21 +8,32 @@ class Expression
 public:
     float eval();
     static Expression parse(std::string s);
+
     enum class op_precedence
     {
         LOW,
         MED,
         HIGH,
-        BRACKET,
         NON_OP
     };
 
+    enum class ops
+    {
+        ADD = '+',
+        SUB = '-',
+        MULT = '*',
+        DIV = '/',
+        OPEN_BRACKET = '(',
+        CLOSED_BRACKET = ')'
+    };
+
 private:
-    Expression(std::string symbol, std::unique_ptr<Expression> &&left, std::unique_ptr<Expression> &&right);
-    static Expression parseRec(const std::string &s);
-    // static Expression parseBracket(const std::string &s);
     std::string symbol;
     std::unique_ptr<Expression> left;
     std::unique_ptr<Expression> right;
+
+    Expression(std::string symbol, std::unique_ptr<Expression> &&left, std::unique_ptr<Expression> &&right);
+
+    static Expression parseRec(const std::string &s);
     static constexpr op_precedence precedence(char ch);
 };
