e6b5775b1b0b36afc20da7c03ea1d75385ec2098
Thomas Briggs <thomas.briggs333@gmail.com>
2021-07-15 18:53:22 +0100

    added precomplied header

--- ChatApp/src/chat_server.cpp
+++ ChatApp/src/chat_server.cpp
@@ -8,14 +8,7 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#include <cstdlib>
-#include <deque>
-#include <iostream>
-#include <list>
-#include <memory>
-#include <set>
-#include <utility>
-#include <asio.hpp>
+#include "precomp.h"
 #include "chat_message.hpp"
 
 using asio::ip::tcp;
@@ -30,7 +23,7 @@ class chat_participant
 {
 public:
   virtual ~chat_participant() {}
-  virtual void deliver(const chat_message& msg) = 0;
+  virtual void deliver(const chat_message &msg) = 0;
 };
 
 typedef std::shared_ptr<chat_participant> chat_participant_ptr;
@@ -43,7 +36,7 @@ public:
   void join(chat_participant_ptr participant)
   {
     participants_.insert(participant);
-    for (auto msg: recent_msgs_)
+    for (auto msg : recent_msgs_)
       participant->deliver(msg);
   }
 
@@ -52,32 +45,35 @@ public:
     participants_.erase(participant);
   }
 
-  void deliver(const chat_message& msg)
+  void deliver(const chat_message &msg)
   {
     recent_msgs_.push_back(msg);
     while (recent_msgs_.size() > max_recent_msgs)
       recent_msgs_.pop_front();
 
-    for (auto participant: participants_)
+    for (auto participant : participants_)
       participant->deliver(msg);
   }
 
 private:
   std::set<chat_participant_ptr> participants_;
-  enum { max_recent_msgs = 100 };
+  enum
+  {
+    max_recent_msgs = 100
+  };
   chat_message_queue recent_msgs_;
 };
 
 //----------------------------------------------------------------------
 
 class chat_session
-  : public chat_participant,
-    public std::enable_shared_from_this<chat_session>
+    : public chat_participant,
+      public std::enable_shared_from_this<chat_session>
 {
 public:
-  chat_session(tcp::socket socket, chat_room& room)
-    : socket_(std::move(socket)),
-      room_(room)
+  chat_session(tcp::socket socket, chat_room &room)
+      : socket_(std::move(socket)),
+        room_(room)
   {
   }
 
@@ -87,7 +83,7 @@ public:
     do_read_header();
   }
 
-  void deliver(const chat_message& msg)
+  void deliver(const chat_message &msg)
   {
     bool write_in_progress = !write_msgs_.empty();
     write_msgs_.push_back(msg);
@@ -102,64 +98,64 @@ private:
   {
     auto self(shared_from_this());
     asio::async_read(socket_,
-        asio::buffer(read_msg_.data(), chat_message::header_length),
-        [this, self](std::error_code ec, std::size_t /*length*/)
-        {
-          if (!ec && read_msg_.decode_header())
-          {
-            do_read_body();
-          }
-          else
-          {
-            room_.leave(shared_from_this());
-          }
-        });
+                     asio::buffer(read_msg_.data(), chat_message::header_length),
+                     [this, self](std::error_code ec, std::size_t /*length*/)
+                     {
+                       if (!ec && read_msg_.decode_header())
+                       {
+                         do_read_body();
+                       }
+                       else
+                       {
+                         room_.leave(shared_from_this());
+                       }
+                     });
   }
 
   void do_read_body()
   {
     auto self(shared_from_this());
     asio::async_read(socket_,
-        asio::buffer(read_msg_.body(), read_msg_.body_length()),
-        [this, self](std::error_code ec, std::size_t /*length*/)
-        {
-          if (!ec)
-          {
-            room_.deliver(read_msg_);
-            do_read_header();
-          }
-          else
-          {
-            room_.leave(shared_from_this());
-          }
-        });
+                     asio::buffer(read_msg_.body(), read_msg_.body_length()),
+                     [this, self](std::error_code ec, std::size_t /*length*/)
+                     {
+                       if (!ec)
+                       {
+                         room_.deliver(read_msg_);
+                         do_read_header();
+                       }
+                       else
+                       {
+                         room_.leave(shared_from_this());
+                       }
+                     });
   }
 
   void do_write()
   {
     auto self(shared_from_this());
     asio::async_write(socket_,
-        asio::buffer(write_msgs_.front().data(),
-          write_msgs_.front().length()),
-        [this, self](std::error_code ec, std::size_t /*length*/)
-        {
-          if (!ec)
-          {
-            write_msgs_.pop_front();
-            if (!write_msgs_.empty())
-            {
-              do_write();
-            }
-          }
-          else
-          {
-            room_.leave(shared_from_this());
-          }
-        });
+                      asio::buffer(write_msgs_.front().data(),
+                                   write_msgs_.front().length()),
+                      [this, self](std::error_code ec, std::size_t /*length*/)
+                      {
+                        if (!ec)
+                        {
+                          write_msgs_.pop_front();
+                          if (!write_msgs_.empty())
+                          {
+                            do_write();
+                          }
+                        }
+                        else
+                        {
+                          room_.leave(shared_from_this());
+                        }
+                      });
   }
 
   tcp::socket socket_;
-  chat_room& room_;
+  chat_room &room_;
   chat_message read_msg_;
   chat_message_queue write_msgs_;
 };
@@ -169,9 +165,9 @@ private:
 class chat_server
 {
 public:
-  chat_server(asio::io_context& io_context,
-      const tcp::endpoint& endpoint)
-    : acceptor_(io_context, endpoint)
+  chat_server(asio::io_context &io_context,
+              const tcp::endpoint &endpoint)
+      : acceptor_(io_context, endpoint)
   {
     do_accept();
   }
@@ -197,7 +193,7 @@ private:
 
 //----------------------------------------------------------------------
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
   try
   {
@@ -218,7 +214,7 @@ int main(int argc, char* argv[])
 
     io_context.run();
   }
-  catch (std::exception& e)
+  catch (std::exception &e)
   {
     std::cerr << "Exception: " << e.what() << "\n";
   }
---
+++ ChatApp/src/precomp.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstdlib>
+#include <deque>
+#include <iostream>
+#include <list>
+#include <memory>
+#include <set>
+#include <utility>
+#include <asio.hpp>
+#include <thread>
