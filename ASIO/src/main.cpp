bdb57b5a04205fd0217184d02213bc6ce7cadc84
Thomas Briggs <thomas.briggs333@gmail.com>
2021-07-15 13:53:17 +0100

    stuff

--- ASIO/src/main.cpp
+++ ASIO/src/main.cpp
@@ -1,5 +1,7 @@
 #include <asio.hpp>
 #include <iostream>
+#define ASIO_STANDALONE
+#define _WIN32_WINNT 0x0A00
 
 //Standard blocking call
 // int main(int argc, char const *argv[])
@@ -13,19 +15,19 @@
 
 
 //ASync clocking call
-// void print(const asio::error_code &/*e*/)
-// {
-//     std::cout << "Hello, World" << '\n';
-// }
+void print(const asio::error_code &/*e*/)
+{
+    std::cout << "Hello, World!" << '\n';
+}
 
-// int main(int argc, char const *argv[])
-// {
-//     asio::io_context io;
-//     asio::steady_timer t(io, asio::chrono::seconds(5));
-//     t.async_wait(print);
-//     io.run();
-//     return 0;
-// }
+int main(int argc, char const *argv[])
+{
+    asio::io_context io;
+    asio::steady_timer t(io, asio::chrono::seconds(5));
+    t.async_wait(print);
+    io.run();
+    return 0;
+}
 
 
 
