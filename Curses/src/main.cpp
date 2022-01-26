1ccb926d6bac7d50120450dedcc8234584022220
Thomas Briggs <thomas.rbiggs333@gmail.com>
2022-01-26 02:36:25 +0000

    attributes

--- Curses/src/main.cpp
+++ Curses/src/main.cpp
@@ -3,12 +3,11 @@
 
 void simpleText() {
     const std::string s = "Hello, World!";
-	printw(s.c_str());	/* Print Hello World */
-	refresh();			/* Print it on to the real screen */
-    move(1, 0);         /* Moves the cursor */ 
-	int i = getch();	/* Wait for user input */
-	printw("%d", i);
-    getch();
+    printw(s.c_str());	/* Print Hello World */
+    refresh();			/* Print it on to the real screen */
+    move(1, 0);         /* Moves the cursor */
+    int i = getch();	/* Wait for user input */
+    printw("%d", i);
 
 }
 
@@ -16,34 +15,73 @@ void simpleWindow() {
 
     int height = 10, width = 20, start_y = 0, start_x = 1;
 
-    WINDOW * win = newwin(height, width, start_y, start_x); /* Creates a new window */
+    WINDOW* win = newwin(height, width, start_y, start_x); /* Creates a new window */
     refresh();
     box(win, 0, 0);                                         /* Draws a box around a window */
     mvwprintw(win, 1, 1, "This is a box!");                 /* Moves cursor relative to window, and prints */
     wrefresh(win);                                          /* Refreshs the window */
-    getch();
 }
 
 void attributres() {
 
-    
+    /* List of available attributes
+     * A_NORMAL
+     * A_STANDOUT
+     * A_REVERSE
+     * A_BLINK
+     * A_DIM
+     * A_BOLD
+     * A_PROTECT
+     * A_INVIS
+     * A_ALTCHARSET
+     * A_CHARTEXT
+     */
+
+    clear();
+    printw("STANDARD");
+
+    int y, x;
+
+    attron(A_STANDOUT);
+    getyx(stdscr, y, x);
+    mvprintw(y+1, 0, "STANDOUT");
+    attroff(A_STANDOUT);
+
+    attron(A_BLINK);
+    getyx(stdscr, y, x);
+    mvprintw(y+1, 0, "BLINK");
+    attroff(A_BLINK);
+
+    attron(A_DIM);
+    getyx(stdscr, y, x);
+    mvprintw(y+1, 0, "DIM");
+    attroff(A_DIM);
+
+
+    attron(A_BOLD);
+    getyx(stdscr, y, x);
+    mvprintw(y+1, 0, "BOLD");
+    attroff(A_BOLD);
+
 
 }
 
 
 int main()
-{	
+{
     //Init curses
-	initscr();			/* Start curses move */
+    initscr();			/* Start curses move */
     noecho();           /* Typed char don't show */
     cbreak();           /* ctrl+c will exit program (default) */
     //raw();            /* Special characters will be ignored */
 
     //Simple curses programs
     // simpleText();
-    simpleWindow();
+    // simpleWindow();
+    attributres();
 
     //Clearn up after curses
+    getch();
     endwin();			/* End curses mode */
 }
 
