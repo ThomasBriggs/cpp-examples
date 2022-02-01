4f70ad64018c2b0ffc4f1f8bdc0649d20a3e7107
Thomas Briggs <thomas.rbiggs333@gmail.com>
2022-02-01 23:27:20 +0000

    added results utils

--- typer-racer/src/util.cpp
+++ typer-racer/src/util.cpp
@@ -1,2 +1,12 @@
-#include <string>
+#include "util.h"
 
+float getAccuracy(std::vector<std::string> input, std::vector<std::string> goal)
+{
+    size_t denom = input.size();
+    size_t correct = 0;
+    for (size_t i = 0; i < input.size(); i++)
+    {
+        correct = input[i] == goal[i] ? correct + 1 : correct + 0;
+    }
+    return correct/denom;
+}
