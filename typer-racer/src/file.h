017f09eb5d514878489976291f72e36241f6f451
Thomas Briggs <thomas.briggs333@gmail.com>
2022-03-22 16:45:48 +0000

    Moved typer-racer to it's own repo

--- typer-racer/src/file.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> getRandomWords(const std::string& f, u_short n);
-std::vector<std::string> getAllWords(const std::string& f);
